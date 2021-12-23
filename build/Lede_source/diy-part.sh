0762be0f92d633808d20511f9109b5395a948972
mic0828 <96179159+mic0828@users.noreply.github.com>
2021-12-23 18:16:44 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -19,6 +19,7 @@ uci set network.lan.dns='223.5.5.5'                                         # DN
 uci set network.lan.delegate='0'                                            # 去掉LAN口使用内置的 IPv6 管理
 uci commit network                                                          # 不要删除跟注释,除非上面全部删除或注释掉了
 uci set system.@system[0].hostname='OpenWrt-K2P'                            # 修改主机名称为OpenWrt-K2P
+sed -i 's/\/bin\/login/\/bin\/login -f root/' /etc/config/ttyd              # 设置ttyd免帐号登录，如若开启，进入OPENWRT后可能要重启一次才生效
 EOF
 
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ                                                             # 设置密码为空
